ad092f63bcbd81e6afb581cfb8166178040febb9
Joanne <truong.j@husky.neu.edu>
2016-04-03 00:19:00 -0400

    styling

--- app/assets/javascripts/fadeMe.js
+++ app/assets/javascripts/fadeMe.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
   $(".fadeMe").hide();
   $(".fadeMe").fadeToggle(555);
+  var $window = $(window);
+  var h = $window.height()*80/100;
+  $(".section1").height(h);
+});
+
+$(window).resize(function() {
+  var $window = $(window);
+  var h = $window.height()*80/100;
+  $(".section1").height(h);
 });
