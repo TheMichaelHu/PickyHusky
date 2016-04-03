b52b9db9b08a29c0aadd1339d670fee3a7eced9c
Michael <michaelanhu@gmail.com>
2016-04-03 07:57:24 -0400

    added text every day

--- app/clock.rb
+++ app/clock.rb
@@ -63,6 +63,27 @@ module Clockwork
       end
     end
 
+    User.each do |user|
+      account_sid = 'AC82d2b9fe40b28afaac752136441b10a7' 
+      auth_token = '56d81ca61c2e2a6c8531b42cc0cdd7ee' 
+       
+      @client = Twilio::REST::Client.new account_sid, auth_token 
+      msg = ""
+      user_foods = user.foods.where(available: true).limit(10)
+      user_foods.each do |food|
+        dining_halls = food.dining_halls.inject { |str, dh| str + ", #{dh}" }
+        meals = food.meals.inject { |str, meal| str + ", #{meal}" }
+        msg += "#{food.name} is available at #{dining_halls} for #{meals}.\n"
+      end
+      unless user.phone_number.nil? or msg.empty?
+        @client.account.messages.create({
+          :from => '+16572346969', 
+          :to => user.phone_number, 
+          :body => msg,  
+        })
+      end
+    end
+
   end
 
   every(1.day, 'main.job')
