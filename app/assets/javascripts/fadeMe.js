d58dc7484bbed2ad1c064a4b5bfbbbbbe8b51294
NaokiCho <naoki.cho7@gmail.com>
2016-04-03 09:25:53 -0400

    refined landing

--- app/assets/javascripts/fadeMe.js
+++ app/assets/javascripts/fadeMe.js
@@ -2,12 +2,21 @@ $(document).ready(function() {
   $(".fadeMe").hide();
   $(".fadeMe").fadeToggle(555);
   var $window = $(window);
-  var h = $window.height()*80/100;
-  $(".section1").height(h);
+  var h = $window.height();
+  var junk = manageHeights(h);
 });
 
 $(window).resize(function() {
   var $window = $(window);
-  var h = $window.height()*80/100;
-  $(".section1").height(h);
+  var h = $window.height();
+  var junk = manageHeights(h);
 });
+
+function manageHeights(h) {
+	$(".section1").height(h*80/100);
+  $(".section3").height(h*75/100);
+  $(".section5").height(h*75/100);
+  $(".section7").height(h);
+  $(".section7").css("padding", h*40/100+"px");
+  return 1;              // The function returns the product of p1 and p2
+}
